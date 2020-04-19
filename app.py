4544d76bb82304bf58b38beb0b2494ad0d66f835
Emmanuel <emsammy12@gmail.com>
2020-04-19 16:24:54 -0400

    routes/ ingredient models started

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from flask_jwt import JWT, jwt_required, current_identity
 from sqlalchemy.exc import IntegrityError
 from datetime import timedelta 
 
-from models import db, User
+from models import db, User, MyIngredients
 
 def create_app():
-  app = Flask(__name__)
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-  app.config['SECRET_KEY'] = "MYSECRET"
-  app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7) 
-  db.init_app(app)
-  return app
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = "MYSECRET"
+    app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7) 
+    db.init_app(app)
+    return app
 
 app = create_app()
 
@@ -21,41 +21,50 @@ app.app_context().push()
 db.create_all(app=app)
 
 def authenticate(uname, password):
-  #search for the specified user
-  user = User.query.filter_by(username=uname).first()
-  #if user is found and password matches
-  if user and user.check_password(password):
-    return user
+    #search for the specified user
+    user = User.query.filter_by(username=uname).first()
+    #if user is found and password matches
+    if user and user.check_password(password):
+        return user
 
 def identity(payload):
-  return User.query.get(payload['identity'])
+    return User.query.get(payload['identity'])
 
 jwt = JWT(app, authenticate, identity)
 
 @app.route('/')
 def hello():
-    return "WEB PROJECT"
+    return render_template('index.html')
 
 @app.route('/register')
 def register():
-    return render_template('signup.html')
+    return render_template('signup.html') 
 
 @app.route('/signup', methods=['POST'])
 def signup():
-  userdata = request.get_json()
-  newuser = User(username=userdata['username'], email=userdata['email'])
-  newuser.set_password(userdata['password'])
-  try:
-    db.session.add(newuser)
-    db.session.commit()
-  except IntegrityError:
-    db.session.rollback()
-    return 'username or email already exists', 401
-  return 'user created', 201
+    userdata = request.get_json()
+    newuser = User(username=userdata['username'], email=userdata['email'])
+    newuser.set_password(userdata['password'])
+    try:
+        db.session.add(newuser)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return 'username or email already exists', 401
+    return 'user created', 201
 
 @app.route('/ingredients')
-def ingredients():
-    return "WEB PROJECT"
+@jwt_required()
+def add_ingredient():
+    userdata = request.get_json()
+    ingredient = MyIngredient(name=userdata['name'])
+    try:
+        db.session.add(ingredient)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return 'Ingredient already added.', 401
+    return ingredient.name + 'added.', 201
 
 
 
--- models.py
+++ models.py
@@ -4,16 +4,22 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
-class MyIngredients(db.Model):
-  iid = db.Column('iid', db.Integer, primary_key=True)
-  id = db.Column('id', db.Integer, db.ForeignKey('user.id'))
-  name = db.Column(db.String(50))
+    def toDict(self):
+        return{
+            'name':self.name,
+            'email':self.email
+    }
 
-  def toDict(self):
+class MyIngredients(db.Model):
+    iid = db.Column('iid', db.Integer, primary_key=True)
+    id = db.Column('id', db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(50))
+
+def toDict(self):
     return{
-      'name':self.name,
+        'name':self.name
     }
